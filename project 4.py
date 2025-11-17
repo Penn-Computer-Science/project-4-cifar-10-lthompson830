c6b8f1124679f59cfb38e9b6d896ac02764b0454
lthompson830 <lthompson830@phm.k12.in.us>
2025-11-17 08:10:49 -0500

    Update project 4.py

--- project 4.py
+++ project 4.py
@@ -54,7 +54,12 @@ model = tf.keras.models.Sequential(
     # ]
 
     [
+    tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
     tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv2D(64, (5,5), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(num_classes, activation='softmax')
     ]
